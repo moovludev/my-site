b371438c7d49660a47a00db6ca4053b6e1592f1c
Moovlu <Epicmoo05@gmail.com>
2025-02-23 22:53:25 +1100

    Cleaned code javascript and html - Cleaned up statuscafe JS code - Made statuscafe code conform to new style - Removed reference to old stylesheet

--- scripts/home.js
+++ scripts/home.js
@@ -1,13 +1,29 @@
-// Load up statuscafe
-// document.writeln('<div id="statuscafe"><div id="statuscafe-username"></div><div id="statuscafe-content"></div></div>');
+// Query Statuscafe and write html
+fetch("https://status.cafe/users/moovlu/status.json")
+  .then((response) => response.json())
+  .then((data) => {
+    const statusUsername = document.getElementById("statuscafe-username");
+    const statusContent = document.getElementById("statuscafe-content");
 
-// fetch("https://status.cafe/users/moovlu/status.json")
-//   .then( r => r.json() )
-//   .then( r => {
-//     document.getElementById("statuscafe-username").innerHTML = '<a href="https://status.cafe/users/moovlu" target="_blank">' + r.author + '</a> ' + r.face + ' ' + r.timeAgo
-//     document.getElementById("statuscafe-content").innerHTML = r.content
-//   })
+    if (statusUsername && statusContent) {
+      statusUsername.innerHTML = `
+        <div class="status-container">
+          <img src="/images/status_avatar_placeholder.png" class="status-image" alt="User Avatar">
+          <span>
+            <a href="https://status.cafe/users/moovlu" target="_blank" rel="noopener noreferrer">
+              <p>${data.author}</p>
+            </a>
+            ${data.face}
+            <p class="subtext">${data.timeAgo}</p>
+          </span>
+        </div>`;
 
+      statusContent.innerHTML = `<p>${data.content}</p>`;
+    }
+  })
+  .catch((error) => {
+    console.error("Error fetching status:", error);
+  });
 
 // Load a random tip
 const tip_list = ["bacon tastes nice", "i have no tips for you :("];
@@ -21,7 +37,6 @@ function writeTip(text) {
 
 // Write in text slowly
 for (let i=0; i <= selected_tip.length; i++) {
-  console.log(i)
   setTimeout(() => {
     writeTip(selected_tip.slice(0,i));
   }, 100 * i);
