419ee4aa28e23c82f86cb8d4b8a7f7a1c1ceeb07
Moovlu <Epicmoo05@gmail.com>
2025-02-24 17:48:20 +1100

    Updated readme, added warnings, cleaned html - Updated readme - Added warning for mobile screens - Relocated preload tags to speed up page load times

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-Temporary readme file, will be updated with proper text later.
+# My Personal Site
+A website I've created entirely from scratch. The live version of this site is hosted on [Neocities](https://moovlu.neocities.org/) while the development version is hosted here on [GitHub Pages](https://moovlu.github.io/my-site/).
+
+I want to ensure as much as possible on this site is created by me. That means:
+- Every line of code is written by me
+- Every asset is made by me
+
+Any instance of something not created by me will be attributed inside of the credits page.
+
+> [!NOTE]
+> If I accidentally miss a reference to any material, please create a GitHub issue or contact me.
+
+## Repository Overview
+- /blog
+  - The blog subdirectory containing all blog pages
+- /design
+  - All relevent design documents used during planning and development
+    - Krita KRA files
+    - Aseprite files
+- /images
+  - All images used
+- /scripts
+  - All JavaScript used
+- /styles
+  - All CSS stylesheets and TTF font files
+
+## Code Usage
+Feel free to use any of my code or assets freely. A reference is always appreciated but not required :)
--- scripts/home.js
+++ scripts/home.js
@@ -1,29 +1,29 @@
 // Query Statuscafe and write html
 fetch("https://status.cafe/users/moovlu/status.json")
-  .then((response) => response.json())
-  .then((data) => {
-    const statusUsername = document.getElementById("statuscafe-username");
-    const statusContent = document.getElementById("statuscafe-content");
+	.then((response) => response.json())
+	.then((data) => {
+		const statusUsername = document.getElementById("statuscafe-username");
+		const statusContent = document.getElementById("statuscafe-content");
 
-    if (statusUsername && statusContent) {
-      statusUsername.innerHTML = `
-        <div class="status-container">
-          <img src="/images/status_avatar_placeholder.png" class="status-image" alt="User Avatar">
-          <span>
-            <a href="https://status.cafe/users/moovlu" target="_blank" rel="noopener noreferrer">
-              <p>${data.author}</p>
-            </a>
-            ${data.face}
-            <p class="subtext">${data.timeAgo}</p>
-          </span>
-        </div>`;
+		if (statusUsername && statusContent) {
+		statusUsername.innerHTML = `
+			<div class="status-container">
+			<img src="/images/status_avatar_placeholder.png" class="status-image" alt="User Avatar">
+			<span>
+				<a href="https://status.cafe/users/moovlu" target="_blank" rel="noopener noreferrer">
+				<p>${data.author}</p>
+				</a>
+				${data.face}
+				<p class="subtext">${data.timeAgo}</p>
+			</span>
+			</div>`;
 
-      statusContent.innerHTML = `<p>${data.content}</p>`;
-    }
-  })
-  .catch((error) => {
-    console.error("Error fetching status:", error);
-  });
+		statusContent.innerHTML = `<p>${data.content}</p>`;
+		}
+	})
+	.catch((error) => {
+		console.error("Error fetching status:", error);
+	});
 
 // Load a random tip
 const tip_list = ["bacon tastes nice", "i have no tips for you :("];
@@ -32,12 +32,19 @@ let random_tip_index = Math.floor(Math.random() * tip_list.length);
 let selected_tip = tip_list[random_tip_index];
 
 function writeTip(text) {
-  document.getElementById("tip").innerHTML = text;
+document.getElementById("tip").innerHTML = text;
 }
 
 // Write in text slowly
-for (let i=0; i <= selected_tip.length; i++) {
-  setTimeout(() => {
-    writeTip(selected_tip.slice(0,i));
-  }, 100 * i);
+for (let i = 0; i <= selected_tip.length; i++) {
+	setTimeout(() => {
+		writeTip(selected_tip.slice(0, i));
+	}, 100 * i);
+}
+
+// Check for smaller screens
+let screenWidth = window.innerWidth;
+
+if (screenWidth < 960) {
+	alert("Your screen is too small. It's recommended you use a desktop to view this site.")
 }
